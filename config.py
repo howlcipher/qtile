df407398c38d2d3bb4963c7f4cf91c0f8ad2137d
William Elias <wylelias.123@gmail.com>
2020-09-26 21:29:14 -0400

    Add files via upload
    config and autostart script

---
+++ config.py
@@ -0,0 +1,332 @@
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from typing import List  # noqa: F401
+
+from libqtile import bar, layout, widget, hook
+from libqtile.config import Click, Drag, Group, Key, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+import os
+import re
+import subprocess
+
+@hook.subscribe.startup_once
+def autostart():
+    home = os.path.expanduser('~/.config/qtile/autostart.sh') # path to my script, under my user directory
+    subprocess.call([home])
+
+
+
+mod = "mod4"
+myTerm = "termite"
+
+keys = [
+     ### The essentials
+         Key([mod], "Return",
+             lazy.spawn(myTerm),
+             desc='Launches My Terminal'
+             ),
+         Key([mod, "shift"], "Return",
+             lazy.spawn("dmenu_run -p 'Run: '"),
+             desc='Dmenu Run Launcher'
+             ),
+         Key([mod], "Tab",
+             lazy.next_layout(),
+             desc='Toggle through layouts'
+             ),
+         Key([mod, "shift"], "c",
+             lazy.window.kill(),
+             desc='Kill active window'
+             ),
+         Key([mod, "shift"], "r",
+             lazy.restart(),
+             desc='Restart Qtile'
+             ),
+         Key([mod, "shift"], "q",
+             lazy.shutdown(),
+             desc='Shutdown Qtile'
+             ),
+         Key(["control", "shift"], "e",
+             lazy.spawn("emacsclient -c -a emacs"),
+             desc='Doom Emacs'
+             ),
+         ### Window controls
+         Key([mod], "k",
+             lazy.layout.down(),
+             desc='Move focus down in current stack pane'
+             ),
+         Key([mod], "j",
+             lazy.layout.up(),
+             desc='Move focus up in current stack pane'
+             ),
+         Key([mod, "shift"], "k",
+             lazy.layout.shuffle_down(),
+             desc='Move windows down in current stack'
+             ),
+         Key([mod, "shift"], "j",
+             lazy.layout.shuffle_up(),
+             desc='Move windows up in current stack'
+             ),
+         Key([mod], "h",
+             lazy.layout.grow(),
+             lazy.layout.increase_nmaster(),
+             desc='Expand window (MonadTall), increase number in master pane (Tile)'
+             ),
+         Key([mod], "l",
+             lazy.layout.shrink(),
+             lazy.layout.decrease_nmaster(),
+             desc='Shrink window (MonadTall), decrease number in master pane (Tile)'
+             ),
+         Key([mod], "n",
+             lazy.layout.normalize(),
+             desc='normalize window size ratios'
+             ),
+         Key([mod], "m",
+             lazy.layout.maximize(),
+             desc='toggle window between minimum and maximum sizes'
+             ),
+         Key([mod, "shift"], "f",
+             lazy.window.toggle_floating(),
+             desc='toggle floating'
+             ),
+         Key([mod, "shift"], "m",
+             lazy.window.toggle_fullscreen(),
+             desc='toggle fullscreen'
+             ),
+         ### Stack controls
+         Key([mod, "shift"], "space",
+             lazy.layout.rotate(),
+             lazy.layout.flip(),
+             desc='Switch which side main pane occupies (XmonadTall)'
+             ),
+         Key([mod], "space",
+             lazy.layout.next(),
+             desc='Switch window focus to other pane(s) of stack'
+             ),
+         Key([mod, "control"], "Return",
+             lazy.layout.toggle_split(),
+             desc='Toggle between split and unsplit sides of stack'
+             ),
+
+]
+
+groups = [Group(i) for i in "asdfuiop"]
+
+for i in groups:
+    keys.extend([
+        # mod1 + letter of group = switch to group
+        Key([mod], i.name, lazy.group[i.name].toscreen(),
+            desc="Switch to group {}".format(i.name)),
+
+        # mod1 + shift + letter of group = switch to & move focused window to group
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(i.name)),
+        # Or, use below if you prefer not to switch to that group.
+        # # mod1 + shift + letter of group = move focused window to group
+        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+        #     desc="move focused window to group {}".format(i.name)),
+    ])
+
+layout_theme = {
+        "font" : "Ubuntu",
+        "fontsize" : 12,
+        "margin" : 15,
+        "border_focus" : "227093",
+        "border_normal" : "84817a",
+        "border_width" : 2           
+           }
+
+layouts = [
+    # layout.Stack(num_stacks=2),
+    # Try more layouts by unleashing below layouts.
+    # layout.Bsp(),
+    # layout.Columns(),
+    # layout.Matrix(),
+    layout.MonadTall(**layout_theme),
+    layout.Max(**layout_theme),
+    # layout.MonadWide(),
+    layout.RatioTile(**layout_theme),
+    layout.Tile(**layout_theme),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font='sans',
+    fontsize=12,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
+
+
+colors = [["#292d3e", "#292d3e"], # panel background - 0
+          ["#ffffff", "#ffffff"], # white - 1
+          ["#e1acff", "#e1acff"], # window name -2
+          ["#6ab04c", "#6ab04c"], #flat dark green - 3
+          ["#273c75", "#273c75"], #flat dark blue - 4
+          ["#000000","#000000"],  #black - 5
+          ["#00a8ff", "#00a8ff"], #light blue - 6
+          ["#badc58", "#badc58"], #light green - 7
+          ["#44bd32", "#44bd32"]] # bright green - 8
+
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.CurrentLayout(foreground=colors[8]),
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Chord(
+                    chords_colors={
+                        'launch': ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                # widget.TextBox("default config", name="default"),
+                widget.TextBox(text = " Network:",
+                                foreground = colors[8],
+                                background = colors[5],
+                                padding = 5
+                ),
+                widget.NetGraph(forground = colors[1]),
+                widget.TextBox(text = " CPU:",
+                                foreground = colors[8],
+                                background = colors[5],
+                                padding = 5
+                ),
+                widget.CPUGraph(),
+                widget.TextBox(text = " Memory:",
+                                foreground = colors[8],
+                                background = colors[5],
+                                padding = 5
+                ),
+                widget.MemoryGraph(),
+                # widget.TextBox("Press &lt;mod+shift+enter&gt; for menu", foreground=colors[3]),
+                widget.Systray(padding=5),
+                widget.Clock(foreground=colors[6], format='%A, %B %d  [ %H:%M ]'),
+                widget.TextBox(text = " Vol:",
+                                foreground = colors[8],
+                                background = colors[5],
+                                padding = 5
+                ),
+                widget.Volume(
+                       foreground = colors[8],
+                       background = colors[5],
+                       padding = 5
+                       ),
+                widget.Battery(foreground = colors[6], format = '{char} {percent:2.0%} {hour:d}:{min:02d}'),
+                widget.QuickExit(foreground = "#7ed6df"),
+            ],
+            24,
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+main = None  # WARNING: this is deprecated and will be removed soon
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(float_rules=[
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    {'wmclass': 'confirm'},
+    {'wmclass': 'dialog'},
+    {'wmclass': 'download'},
+    {'wmclass': 'error'},
+    {'wmclass': 'file_progress'},
+    {'wmclass': 'notification'},
+    {'wmclass': 'splash'},
+    {'wmclass': 'toolbar'},
+    {'wmclass': 'confirmreset'},  # gitk
+    {'wmclass': 'makebranch'},  # gitk
+    {'wmclass': 'maketag'},  # gitk
+    {'wname': 'branchdialog'},  # gitk
+    {'wname': 'pinentry'},  # GPG key password entry
+    {'wmclass': 'ssh-askpass'},  # ssh-askpass
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+
+
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+main = None
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+
+floating_layout = layout.Floating(float_rules=[
+    {'wmclass': 'confirm'},
+    {'wmclass': 'dialog'},
+    {'wmclass': 'download'},
+    {'wmclass': 'error'},
+    {'wmclass': 'file_progress'},
+    {'wmclass': 'notification'},
+    {'wmclass': 'splash'},
+    {'wmclass': 'toolbar'},
+    {'wmclass': 'confirmreset'},  # gitk
+    {'wmclass': 'makebranch'},  # gitk
+    {'wmclass': 'maketag'},  # gitk
+    {'wname': 'branchdialog'},  # gitk
+    {'wname': 'pinentry'},  # GPG key password entry
+    {'wmclass': 'ssh-askpass'},  # ssh-askpass
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+
+
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
